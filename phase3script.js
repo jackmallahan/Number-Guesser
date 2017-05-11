98b16a546be011b1350475250db028908d386bbb
Jack Mallahan <jackmallahan@gmail.com>
2017-05-10 21:54:04 -0600

    Final Product

--- phase3script.js
+++ phase3script.js
@@ -9,73 +9,46 @@ var clearButton = document.querySelector('.clear');
 var resetButton = document.getElementById('reset-button');
 var titleTaunt = document.querySelector('.title')
 var output = document.querySelector('.output');
-var randomNumber = getRandom(min, max);
+var randomNumber = getRandom(parseInt(userMin.value), parseInt(userMax.value));
 var arrayOfGuesses = []
 var min = 1
 var max = 100
+
 
 //Functions
 function logicFunction (){
   var newInput = parseInt(userInput.value);
   if (newInput < parseInt(userMin.value) || newInput > parseInt(userMax.value)){
-    guessedNumber.innerText = "You set the range. You can't make a guess inside it? IDIOT!";
+    titleTaunt.innerText = "You set the range. You can't make a guess inside it?";
+    guessedNumber.innerText = "Guess a number between " + userMin.value + " and " + userMax.value;
     output.innerText = "I Feel Like I'm Taking Crazy Pills!";
   }else if (newInput > randomNumber){
     output.innerText = "Which is Too High, You Freaking IDIOT";
   } else if (newInput < randomNumber){
     output.innerText = "Which is Too Low, Ya Dump";
-  } else if (newInput === randomNumber){
-    output.innerText = "Well, Shit. You Win.";
   } else if (isNaN(newInput)){
     guessedNumber.innerText = "Not a Number, Dummy!";
     output.innerText = "Try Guessing a Number...";
+  } else if (newInput === randomNumber){
+      return userWin();
+    }
+
+function userWin(){
+  var newUserMin = parseInt(userMin.value) - 10;
+  var newUserMax = parseInt(userMax.value) + 10;
+  var newRandom = getRandom(newUserMin, newUserMax);
+  userMin.value = newUserMin;
+  userMax.value = newUserMax;
+    output.innerText = "Congrats. You Beat Me. I think its a fluke. Guess again between " + newUserMin + " and " + newUserMax;
+    console.log(newUserMin, newUserMax);
+    console.log(newRandom);
+    randomNumber = newRandom;
   }
 };
+
 function getRandom(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
 };
-
-//User Clicks 'Submit'
-submitButton.addEventListener('click', function(){
-  randomNumber = getRandom(parseInt(userMin.value), parseInt(userMax.value));
-  toggleSubmitButton();
-  console.log(randomNumber)
-})
-
-//User Clicks 'Guess'
-guessButton.addEventListener('click', function() {
-  guessedNumber.innerText = userInput.value;
-  titleTaunt.innerText = "Your Last Guess Was..."
-  logicFunction();
-  arrayOfGuesses.push(userInput.value);
-  toggleResetButton();
-});
-
-//Clear button
-clearButton.addEventListener('click', function(){
-  userInput.value = "";
-  toggleClearButton();
-  toggleGuessButton();
-});
-
-//Reset button
-resetButton.addEventListener('click', function(){
-  location.reload();
-});
-
-//toggle buttons
-userInput.addEventListener('input', function(){
-  toggleGuessButton();
-  toggleClearButton();
-});
-
-userMax.addEventListener('input', function(){
-  toggleSubmitButton();
-})
-
-userMin.addEventListener('input', function(){
-  toggleSubmitButton();
-})
 
 function toggleSubmitButton(){
   if (userMin.value && userMax.value === ''){
@@ -101,3 +74,42 @@ function toggleResetButton(){
     resetButton.disabled = false;
   } else {resetButton.disabled = true};
 }
+
+//User Interaction
+submitButton.addEventListener('click', function(){
+  randomNumber = getRandom(parseInt(userMin.value), parseInt(userMax.value));
+  toggleSubmitButton();
+  console.log(randomNumber)
+})
+
+guessButton.addEventListener('click', function() {
+  guessedNumber.innerText = userInput.value;
+  titleTaunt.innerText = "Your Last Guess Was..."
+  logicFunction();
+  arrayOfGuesses.push(userInput.value);
+  toggleResetButton();
+  userInput.value = ""
+});
+
+clearButton.addEventListener('click', function(){
+  userInput.value = "";
+  toggleClearButton();
+  toggleGuessButton();
+});
+
+resetButton.addEventListener('click', function(){
+  location.reload();
+});
+
+userInput.addEventListener('input', function(){
+  toggleGuessButton();
+  toggleClearButton();
+});
+
+userMax.addEventListener('input', function(){
+  toggleSubmitButton();
+})
+
+userMin.addEventListener('input', function(){
+  toggleSubmitButton();
+})
